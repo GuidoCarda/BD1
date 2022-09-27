fcfbc649146f8c442b9e09c850b4616ec9c19370
Guido Cardarelli <guidocarda@hotmail.com>
2022-09-26 23:14:26 -0300

    =

--- practicaTiposDeDatos/tienda.sql
+++ practicaTiposDeDatos/tienda.sql
@@ -8,6 +8,7 @@ CREATE TABLE gamas (
 
 CREATE TABLE productos (
   id INT PRIMARY KEY AUTO_INCREMENT,
+  codigo_prod VARCHAR(10),
   nombre VARCHAR(50),
   dimensiones VARCHAR(50),
   stock INT,
@@ -24,7 +25,7 @@ CREATE TABLE oficinas (
 )
 
 CREATE TABLE vendedores (
-  id INT PRIMARY KEY AUTO_INCREMENT,
+  id INT PRIMARY KEY,
   nombre VARCHAR(50),
   telefono VARCHAR(8),
   ciudad VARCHAR(50),
@@ -62,3 +63,5 @@ CREATE TABLE pedidos_productos (
   FOREIGN KEY (producto_id) REFERENCES productos(id),
   FOREIGN KEY (pedido_id) REFERENCES pedidos(id)
 )
+
+INSERT INTO producto (codigo_prod, nombre,gama_id,dimensiones,precio) VALUES ('11679','Sierra de Poda 400MM',20,'0,258',1500.15),('21636','Pala',20,'0,156',125.50),('22225','Rastrillo de Jardín',20,'1,064',590), ('AR-001','Ajedrea',40,'15-20',530.50), ('AR-002','Lavándula Dentata',30,'15,20',550), ('AR-008','Thymus Citriodra (Tomillo limón)',30,'1,064', 890.50), ('FR-100','Nectarina',40,'8/10',1000.60), ('FR-16','Calamondin Copa EXTRA Con FRUTA',40,'100-120',990)
