3ae3f1782681e0b64e4710c3afdc62f8bedbd148
Guido Cardarelli <guidocarda@hotmail.com>
2022-09-27 23:50:09 -0300

    first queries

--- practicaTiposDeDatos/tienda.sql
+++ practicaTiposDeDatos/tienda.sql
@@ -65,3 +65,38 @@ CREATE TABLE pedidos_productos (
 )
 
 INSERT INTO producto (codigo_prod, nombre,gama_id,dimensiones,precio) VALUES ('11679','Sierra de Poda 400MM',20,'0,258',1500.15),('21636','Pala',20,'0,156',125.50),('22225','Rastrillo de Jardín',20,'1,064',590), ('AR-001','Ajedrea',40,'15-20',530.50), ('AR-002','Lavándula Dentata',30,'15,20',550), ('AR-008','Thymus Citriodra (Tomillo limón)',30,'1,064', 890.50), ('FR-100','Nectarina',40,'8/10',1000.60), ('FR-16','Calamondin Copa EXTRA Con FRUTA',40,'100-120',990)
+
+INSERT INTO pedidos_productos  VALUES (1,1, 10, 5,70.3,1), (2,3, 10, 3,70.3,2), (3,3,10,40,43.50,3),(4,1, 8, 5,76.3,1),(5,2, 8, 5,70.3,2), (6,1, 3, 5,70.3,1), (7,7, 3, 5,78.3,2), (8,6, 3, 5,70.3,3),(9,6, 1, 5,170.3,1),(10,1, 1, 5,90.3,2);
+
+
+
+-- 1.Devuelve un listado con el código de oficina y la ciudad de la misma. 
+
+SELECT id AS codigo, ciudad from oficinas 
+
+-- 2. Devuelve un listado con la ciudad y el teléfono de las oficinas de Rosario. 
+
+SELECT ciudad, telefono from oficinas WHERE ciudad = 'Rosario'
+SELECT ciudad, telefono from oficinas WHERE ciudad like '%rosario%'
+
+-- 3. Devuelve un listado con el nombre y telefono de los empleados cuyo jefe tiene un código de jefe igual a 1. 
+
+SELECT nombre, telefono FROM vendedores WHERE jefe_id = 1
+
+-- 4. Devuelve un listado con el nombre de los todos los clientes rosarinos.
+SELECT nombre FROM clientes WHERE ciudad like '%Ros%'
+
+
+-- 5. Devuelve un listado con el nombre de los todos los clientes cuyo límite de crédito esta entre 10 y 300.
+
+SELECT nombre FROM clientes WHERE (limite_cred > 10 AND limite_cred < 300)
+
+-- 6. Mostrar los pedidos hechos en el  2008. 
+SELECT * FROM pedidos WHERE fecha like '%2008%'
+
+-- 7. Devuelve un listado con el código de pedido, nombre del cliente y fecha del pedido.
+SELECT p.id AS codigoPedido, cl.nombre,p.fecha AS fechaPedido 
+FROM pedidos p
+
+INNER JOIN clientes cl 
+ON p.cliente_id = cl.id 
